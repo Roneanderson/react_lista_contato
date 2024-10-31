5a64981ec6c44fe03de6e46e837e0c23fc6a1e44
Rone <roneanderson469@gmail.com>
2024-10-31 09:06:05 -0300

    componetizando filtroCard

--- src/containers/Lateral/index.tsx
+++ src/containers/Lateral/index.tsx
@@ -7,12 +7,12 @@ const Lateral = () => (
     <div>
       <S.Campo type="text" placeholder="Buscar" />
       <S.Filtros>
-        <FiltroCard />
-        <FiltroCard />
-        <FiltroCard />
-        <FiltroCard />
-        <FiltroCard />
-        <FiltroCard ativo />
+        <FiltroCard contador={1} legenda={'pendentes'} />
+        <FiltroCard contador={2} legenda={'concluidas'} />
+        <FiltroCard contador={3} legenda={'urgentes'} />
+        <FiltroCard contador={4} legenda={'importantes'} />
+        <FiltroCard contador={5} legenda={'normal'} />
+        <FiltroCard ativo contador={10} legenda={'todas'} />
       </S.Filtros>
     </div>
   </S.Aside>
