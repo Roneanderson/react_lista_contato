f6970f9275d9e34337da3200ad562c7a131e61d3
Rone <roneanderson469@gmail.com>
2024-10-31 08:54:07 -0300

    criando components

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,18 @@
 import {} from 'react-redux'
+import EstiloGlobal, { Container } from './styles'
+import Lateral from './containers/Lateral'
+import ListaDeTarefas from './containers/ListaDeTarefas'
 
 function App() {
-  return <div>ola react</div>
+  return (
+    <>
+      <EstiloGlobal />
+      <Container>
+        <Lateral />
+        <ListaDeTarefas />
+      </Container>
+    </>
+  )
 }
 
 export default App
---
+++ src/containers/ListaDeTarefas/index.tsx
@@ -0,0 +1,9 @@
+const ListaDeTarefas = () => (
+  <main>
+    <p>
+      2 tarefas marcada como: &quot;categoria&ldquo; e &quot;categoria&ldquo;
+    </p>
+  </main>
+)
+
+export default ListaDeTarefas
